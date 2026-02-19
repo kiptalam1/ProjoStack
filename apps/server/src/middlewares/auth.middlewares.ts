39dca02eefc970c99c6fa2a15af289331cbb239d
kiptalam1 <adamskiptalam0@gmail.com>
2026-02-19 10:46:47 +0300

    implement a middleware function to check if user is admin

--- apps/server/src/middlewares/auth.middlewares.ts
+++ apps/server/src/middlewares/auth.middlewares.ts
@@ -1,6 +1,30 @@
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { PayloadSchema } from "@projo/contracts";
+
+// ensure user is admin
+export async function isAdmin(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<Response | void> {
+  try {
+    const role = req.user?.role;
+    if (!role) {
+      return res.status(401).json({ error: "Unauthorized!" });
+    }
+    if (role !== "ADMIN") {
+      return res.status(403).json({ error: "Unauthorized!" });
+    }
+    next();
+  } catch (error: unknown) {
+    const msg = error instanceof Error ? error.message : String(error);
+    console.error(msg);
+    res.status(403).json({ error: "Unauthorized!" });
+  }
+}
+
+// ensure user is logged in;
 export async function isAuthenticated(
   req: Request,
   res: Response,
