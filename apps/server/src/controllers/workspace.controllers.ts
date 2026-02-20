1a665e9ddb697cb739e1dc4ed854382b102392d1
kiptalam1 <adamskiptalam0@gmail.com>
2026-02-20 12:56:02 +0300

    feat: add function and routes for ws-owners to update their workspace name

--- apps/server/src/controllers/workspace.controllers.ts
+++ apps/server/src/controllers/workspace.controllers.ts
@@ -3,6 +3,56 @@ import { prisma } from "../lib/prisma.js";
 import { WorkspaceSchema, type WorkspaceData } from "@projo/contracts";
 import * as z from "zod";
 
+// UPDATE workspace;
+export async function updateWorkspace(
+  req: Request,
+  res: Response,
+): Promise<Response> {
+  try {
+    const user = req.user;
+    const workspaceId = req.params.workspaceId as string;
+    // validate user input
+    const parsed = WorkspaceSchema.safeParse(req.body);
+    if (!parsed.success) {
+      const { formErrors, fieldErrors } = z.flattenError(parsed.error);
+      const messages = [
+        ...Object.values(fieldErrors).flat(),
+        ...Object.values(formErrors).flat(),
+      ];
+      return res.status(400).json({ error: messages });
+    }
+    const name = parsed.data.name.trim();
+
+    // ensure user is valid;
+    if (!user?.id) {
+      return res.status(401).json({ error: "Unauthorized!" });
+    }
+    // ensure workspace id is valid;
+    if (!workspaceId) {
+      return res.status(400).json({ error: "Invalid workspace ID!" });
+    }
+    const updatedWorkspace = await prisma.workspace.update({
+      where: {
+        id: workspaceId,
+        creatorId: String(user.id),
+      },
+      data: {
+        name: name,
+      },
+    });
+
+    // return workspace in response;
+    return res.status(200).json({
+      message: "Workspace updated successfully.",
+      data: updatedWorkspace,
+    });
+  } catch (error: unknown) {
+    const msg = error instanceof Error ? error.message : String(error);
+    console.error(msg);
+    return res.status(500).json({ error: "Something went wrong." });
+  }
+}
+
 /* DELETE a workspace;
  * only admin and workspace-creator can delete
  * all members should be removed from workspace
@@ -18,6 +68,10 @@ export async function deleteWorkspace(
     const isAdmin = user?.role === "ADMIN";
     if (!user?.id) {
       return res.status(401).json({ error: "Unauthorized!" });
+    }
+    // ensure workspace id is valid;
+    if (!workspaceId) {
+      return res.status(400).json({ error: "Invalid workspace ID!" });
     }
 
     const workspace = await prisma.workspace.findUnique({
@@ -64,17 +118,17 @@ export async function getUserWorkspaces(
     let workspaces;
     if (user.role !== "ADMIN") {
       workspaces = await prisma.workspace.findMany({
-				where: {
-					creatorId: user.id,
-				},
-				include: {
-					members: true,
-				},
-			});
+        where: {
+          creatorId: user.id,
+        },
+        include: {
+          members: true,
+        },
+      });
     } else {
       workspaces = await prisma.workspace.findMany({
-				include: { members: true },
-			});
+        include: { members: true },
+      });
     }
     return res.status(200).json({ data: workspaces });
   } catch (error) {
@@ -106,26 +160,26 @@ export async function createWorkspace(
     const { name } = parsed.data satisfies WorkspaceData;
     // check if user has a similar workspace;
     const exist = await prisma.workspace.findFirst({
-			where: { name, creatorId: req.user.id },
-			select: { id: true },
-		});
+      where: { name, creatorId: req.user.id },
+      select: { id: true },
+    });
     if (exist) {
-			return res.status(409).json({ error: "This name is in use." });
-		}
+      return res.status(409).json({ error: "This name is in use." });
+    }
     // create the workspace;
     const newWorkspace = await prisma.$transaction(async (tx) => {
-			const ws = await tx.workspace.create({
-				data: { name: name, creatorId: req.user!.id },
-			});
-			await tx.workspaceMember.create({
-				data: {
-					userId: ws.creatorId,
-					workspaceId: ws.id,
-					memberRole: "OWNER",
-				},
-			});
-			return ws;
-		});
+      const ws = await tx.workspace.create({
+        data: { name: name, creatorId: req.user!.id },
+      });
+      await tx.workspaceMember.create({
+        data: {
+          userId: ws.creatorId,
+          workspaceId: ws.id,
+          memberRole: "OWNER",
+        },
+      });
+      return ws;
+    });
 
     return res
       .status(201)
--- apps/server/src/routes/workspace.routes.ts
+++ apps/server/src/routes/workspace.routes.ts
@@ -3,6 +3,7 @@ import {
   createWorkspace,
   deleteWorkspace,
   getUserWorkspaces,
+  updateWorkspace,
 } from "../controllers/workspace.controllers.js";
 import { isAuthenticated } from "../middlewares/auth.middlewares.js";
 
@@ -11,4 +12,5 @@ const router = express.Router();
 router.post("/create", isAuthenticated, createWorkspace);
 router.get("/", isAuthenticated, getUserWorkspaces);
 router.delete("/delete/:workspaceId", isAuthenticated, deleteWorkspace);
+router.put("/update/:workspaceId", isAuthenticated, updateWorkspace);
 export default router;
