ab958fae6b35e138acdbb9284f9ad214b10452f0
kiptalam1 <adamskiptalam0@gmail.com>
2026-02-24 10:42:11 +0300

    feat: add function and endpoint to fetch workspace tasks.

--- apps/server/src/controllers/task.controllers.ts
+++ apps/server/src/controllers/task.controllers.ts
@@ -2,6 +2,79 @@ import type { Request, Response } from "express";
 import { prisma } from "../lib/prisma.js";
 import { TaskSchema } from "@projo/contracts";
 import * as z from "zod";
+
+// FETCH all tasks in a project;
+export async function getAllTasks(req:Request, res:Response):Promise<Response> {
+  try {
+    const user = req.user;
+    const projectId = req.params.projectId as string;
+
+    if(!projectId) {
+      return res.status(400).json({error: "Invalid project ID!"});
+    }
+
+    if (!user?.id){
+      return res.status(401).json({
+        error: "Unauthorized!"
+      })
+    }
+    
+    // ensure project exists;
+    const project = await prisma.project.findUnique({
+      where: { id: projectId},
+      select: {
+        workspaceId: true,
+        id: true,
+      }
+    });
+
+    if (!project){
+      return res.status(404).json({
+        error: "Project not found."
+      })
+    }
+
+    // check if workspace exists and user is member;
+    const workspace = await prisma.workspace.findUnique({
+      where: {
+        id: project.workspaceId,
+      },
+      select: {
+        creatorId: true,
+        members: {
+          select: {
+            userId: true,
+          },
+        },
+      },
+    });
+    if (!workspace) {
+      return res.status(404).json({ error: "Workspace not found." });
+    }
+    const isMember =
+      workspace.creatorId === user?.id ||
+      workspace.members.some((m) => m.userId === user?.id);
+    if (!isMember) {
+      return res.status(403).json({ error: "You are not a member!" });
+    }
+    // fetch tasks and return in response;
+    const tasks = await prisma.task.findMany({
+      where: {
+        projectId: project.id,
+
+      }
+    })
+
+    return res.status(200).json({
+      data: tasks
+    })
+  } catch (error:unknown) {
+    const msg = error instanceof Error ? error.message : String(error);
+    console.error(msg);
+    return res.status(500).json({error: "Something went wrong."})
+  }  
+}
+
 // CREATE a task
 // any workspace member can create task
 // tasks begin as pending by default
--- apps/server/src/routes/task.routes.ts
+++ apps/server/src/routes/task.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.middlewares.js";
-import { createTask } from "../controllers/task.controllers.js";
+import { createTask, getAllTasks } from "../controllers/task.controllers.js";
 
 const router = express.Router();
 
 router.post("/project/:projectId", isAuthenticated, createTask);
+router.get("/project/:projectId", isAuthenticated, getAllTasks);
 
 export default router;
