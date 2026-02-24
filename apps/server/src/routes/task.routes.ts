fc1187e1e3e1f3a3a8e4077937da6b3adf618b17
kiptalam1 <adamskiptalam0@gmail.com>
2026-02-24 11:10:13 +0300

    feat: add function and route to delete a task

--- apps/server/src/routes/task.routes.ts
+++ apps/server/src/routes/task.routes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.middlewares.js";
-import { createTask, getAllTasks } from "../controllers/task.controllers.js";
+import {
+  createTask,
+  deleteATask,
+  getAllTasks,
+} from "../controllers/task.controllers.js";
 
 const router = express.Router();
 
 router.post("/project/:projectId", isAuthenticated, createTask);
 router.get("/project/:projectId", isAuthenticated, getAllTasks);
+router.delete("/project/:projectId/task/:taskId", isAuthenticated, deleteATask);
 
 export default router;
