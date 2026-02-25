2e85d7aea413fa20b3c385c2aeab569f8b46b43f
kiptalam1 <adamskiptalam0@gmail.com>
2026-02-25 14:09:27 +0300

    initial frontend project installs and setup

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,15 @@
-
-import './App.css'
+import { Routes, Route } from "react-router";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-   <>
-    <h1>
-      hello world!
-    </h1>
-   </>
+    <>
+      <h1>hello</h1>
+      <Routes>
+
+        <Route path="auth/login" element={<LoginPage />} />
+        {/*<Route path="auth/register" element={<Register />} />*/}
+      </Routes></>
   )
 }
 
