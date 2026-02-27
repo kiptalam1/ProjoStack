e714c634a7e0010af68062a9343410250b295cfa
kiptalam1 <adamskiptalam0@gmail.com>
2026-02-27 15:24:57 +0300

    implement mobile sidebar

--- apps/web/src/components/navigation/sidebar.tsx
+++ apps/web/src/components/navigation/sidebar.tsx
@@ -1,33 +1,50 @@
 import { NavLink } from "react-router";
 
-import { SidebarOpen, SidebarClose, Settings, LayoutGrid, FolderKanban, Handshake } from "lucide-react";
+import { X, SidebarOpen, SidebarClose, Settings, LayoutGrid, FolderKanban, Handshake } from "lucide-react";
 import type { Dispatch, SetStateAction } from "react";
 
 
 type SidebarPropType = {
   showSidebar: boolean;
   setShowSidebar: Dispatch<SetStateAction<boolean>>
+  onNavigate?: () => void;
+  onClose?: () => void;
+  variant: "desktop" | "mobile"
 }
 
 
-export default function Sidebar({ showSidebar, setShowSidebar }: SidebarPropType) {
+export default function Sidebar({ showSidebar, setShowSidebar, onNavigate, onClose, variant }: SidebarPropType) {
   const toggleSidebar = () => setShowSidebar((prev) => !prev);
 
   const base = `flex items-center gap-3 hover:bg-sidebar-active w-full border border-transparent  px-4 py-2 rounded-md transition-colors ${showSidebar ? "justify-start" : "justify-center"}`
   const active = "bg-sidebar-active border border-sidebar-active-border text-primary"
   const inactive = "hover:border-slate-700"
 
+
+
+
   return (
     <>
       <aside className="relative flex flex-col bg-sidebar text-sm text-sidebar-text h-screen overflow-y-auto p-4 md:p-6 border-r border-border">
+        {variant === "desktop" && (
+          <button
+            onClick={toggleSidebar}
+            aria-label={showSidebar ? "Collapse sidebar" : "Expand sidebar"}
+            className={`absolute right-2 top-2 px-3 py-2 text-sidebar-text hover:text-muted hover:bg-sidebar-active transition-all duration-100 cursor-pointer rounded-sm ${showSidebar ? "hover:cursor-w-resize" : "hover:cursor-e-resize"}`}>
+            {showSidebar ? <SidebarClose size={22} /> : <SidebarOpen size={22} />}
 
-        <button
-          onClick={toggleSidebar}
-          aria-label={showSidebar ? "Collapse sidebar" : "Expand sidebar"}
-          className={`absolute right-2 top-2 px-3 py-2 text-sidebar-text hover:text-muted hover:bg-sidebar-active transition-all duration-100 cursor-pointer rounded-sm ${showSidebar ? "hover:cursor-w-resize" : "hover:cursor-e-resize"}`}>
-          {showSidebar ? <SidebarClose size={22} /> : <SidebarOpen size={22} />}
+          </button>
 
-        </button>
+
+        )}
+
+        {variant === "mobile" && (
+          <button className="absolute right-2 top-2 p-2  text-red-300"
+            onClick={onClose}
+            aria-label="Close sidebar">
+            <X size={22} />
+          </button>
+        )}
 
 
         {/* Brand / spacer */}
@@ -37,7 +54,7 @@ export default function Sidebar({ showSidebar, setShowSidebar }: SidebarPropType
         </div>
 
         <nav className="mt-4 flex flex-col gap-3">
-          <NavLink to={"/workspaces"}
+          <NavLink to={"/workspaces"} onClick={onNavigate}
             className={({ isActive }) =>
               `group relative ${base} ${isActive ? active : inactive}`}
           >
@@ -45,7 +62,7 @@ export default function Sidebar({ showSidebar, setShowSidebar }: SidebarPropType
             <span className={showSidebar ? "block" : "hidden"}> Workspaces</span>
           </NavLink>
 
-          <NavLink to={"/projects"}
+          <NavLink to={"/projects"} onClick={onNavigate}
             className={({ isActive }) =>
               `${base} ${isActive ? active : inactive}`}
           ><FolderKanban size={18} className="shrink-0" />
@@ -57,7 +74,7 @@ export default function Sidebar({ showSidebar, setShowSidebar }: SidebarPropType
 
 
         <div className="mt-auto pt-2">
-          <NavLink to={"/settings"}
+          <NavLink to={"/settings"} onClick={onNavigate}
             className={({ isActive }) =>
               `${base} ${isActive ? active : inactive}`}
           ><Settings size={18} className="shrink-0" />
--- apps/web/src/layouts/AppLayout.tsx
+++ apps/web/src/layouts/AppLayout.tsx
@@ -1,15 +1,56 @@
 import { useState } from "react";
 import Sidebar from "../components/navigation/sidebar"
+import { Menu, } from "lucide-react";
+import { Outlet } from "react-router";
 
 export default function AppLayout() {
   const [showSidebar, setShowSidebar] = useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
 
   return (
     <>
-      <div className={`relative w-full min-h-screen mx-auto grid transition-[grid-template-columns] ease-in-out duration-300 ${showSidebar ? "grid-cols-[240px_1fr]" : "grid-cols-[72px_1fr]"}`}>
+      <div
+        className={`
+        relative w-full min-h-screen mx-auto md:grid md:transition-[grid-template-columns] md:ease-in-out md:duration-300 ${showSidebar ? "md:grid-cols-[240px_1fr]" : "md:grid-cols-[72px_1fr]"}`}>
 
+        {/* desktop */}
+        <div className="hidden md:block">
+          <Sidebar
+            showSidebar={showSidebar}
+            setShowSidebar={setShowSidebar}
+            variant="desktop"
+          />
+        </div>
 
-        <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+        {mobileOpen && (
+          <>
+            <button
+              className="fixed inset-0 bg-black/50 md:hidden"
+              aria-label="close sidebar"
+              onClick={() => setMobileOpen(false)}
+            />
+            <div className="fixed  w-72 top-0 left-0 h-screen z-50 md:hidden" >
+              <Sidebar
+                showSidebar={true}
+                setShowSidebar={setShowSidebar}
+                variant="mobile"
+                onNavigate={() => setMobileOpen(false)}
+                onClose={() => setMobileOpen(false)}
+              />
+            </div>
+          </>
+        )}
+
+        <main className="min-w-0 overflow-y-auto p-4 sm:p-6">
+
+          <button
+            className="md:hidden mb-4 inline-flex items-center rounded-md border border-muted p-2 "
+            onClick={() => setMobileOpen(true)}
+          >
+            <Menu size={22} />
+          </button>
+          <Outlet />
+        </main>
       </div>
     </>
   )
