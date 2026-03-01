39f4682a3d9e30427e3efc6f48e95ad21c63d28c
kiptalam1 <adamskiptalam0@gmail.com>
2026-03-01 16:55:10 +0300

    more workspace ui refinement

--- apps/web/src/pages/WorkspacesPage.tsx
+++ apps/web/src/pages/WorkspacesPage.tsx
@@ -44,9 +44,9 @@ export default function WorkspacesPage() {
                 to={`/workspace/${w.id}`}
                 key={w.id}
                 className="p-4 bg-card shadow-md shadow-gray-200 rounded-2xl border-2 border-transparent hover:border-2 hover:border-border transition-colors duration-150">
-                <div className="flex flex-col gap-3 items-start justify-between flex-wrap">
+                <div className="flex flex-col gap-3 items-start justify-between flex-wrap ">
                   <div className="w-full flex items-start justify-between gap-3">
-                    <h3 className="text-base font-semibold">{w.name}</h3>
+                    <h3 className="text-base font-semibold  overflow-hidden whitespace-nowrap text-ellipsis">{w.name}</h3>
                     <p className="text-xs border border-border py-0.5 px-1 rounded-full">{(wsUser?.memberRole ?? "MEMBER").toLocaleLowerCase()}</p>
                   </div>
                   <p className={`text-xs ${wsUser?.status === "ACTIVE" ? "text-success" : ""} ${wsUser?.status === "REMOVED" ? "text-danger" : ""} ${wsUser?.status === "SUSPENDED" ? "text-yellow-500" : ""} ${wsUser?.status === "INVITED" ? "text-blue-500" : ""}`}>{wsUser?.status ?? "-"}
@@ -54,7 +54,7 @@ export default function WorkspacesPage() {
                   <p className="text-sm">
                     {w.members.length} {w.members.length === 1 ? "member" : "members"}
                   </p>
-                  <p className="text-xs font-jetbrains">
+                  <p className="text-xs font-jetbrains min-h-10">
                     <span className="text-xs font-inter">Joined: </span>
                     {joinedDate ?? "-"}
                   </p>
