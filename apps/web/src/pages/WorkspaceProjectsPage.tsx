daceede3d159ea32b98610b650dff02f3afc090e
kiptalam1 <adamskiptalam0@gmail.com>
2026-03-06 19:31:51 +0300

    create a modal for project creation

--- apps/web/src/pages/WorkspaceProjectsPage.tsx
+++ apps/web/src/pages/WorkspaceProjectsPage.tsx
@@ -2,9 +2,12 @@ import { Link, useParams } from "react-router"
 import { useAuth } from "../auth/useAuth"
 import { Loader2 } from "lucide-react";
 import { useGetProjects } from "../features/projects/hooks/useProjects";
+import { Activity, useState } from "react";
+import CreateProjectModal from "../components/modals/CreateProjectModal";
 
 export default function WorkspaceProjectsPage() {
   const { loading } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
   const { workspaceId } = useParams();
   const { isPending, data, isError, error } = useGetProjects(workspaceId as string)
 
@@ -34,45 +37,52 @@ export default function WorkspaceProjectsPage() {
 
 
   return (
-    <div className="w-full h-full space-y-4">
-      <div className="space-y-5">
-        <h1 className="text-2xl font-bold">Projects</h1>
-        <button type="button" className="bg-primary rounded-xl px-3 py-1 cursor-pointer text-sm text-white hover:opacity-80 transition-all duration-150">Create Project
-        </button>
-        {
-          data.length > 0 &&
-          <p className="text-xs">Select a project to continue</p>
-        }
-      </div>
-      <div className="grid gap-3 sm:grid-cols-2 md:grid-cols-3">
-        {
-          data?.map((p) => {
-            const joinedDate = p.createdAt
-              ? formatter.format(new Date(p.createdAt)) : null;
-            return (
-              <Link
-                to={`/workspaces/${p.workspaceId}/projects/${p.id}/tasks`}
-                key={p.id}
-                className="p-4 bg-card shadow-md shadow-gray-200 rounded-2xl border-2 border-transparent hover:border-2 hover:border-border transition-colors duration-150">
-                <div className="flex flex-col gap-3 items-start justify-between flex-wrap ">
-                  <div className="w-full flex items-start justify-between gap-3">
-                    <h3 className="text-base font-semibold  overflow-hidden whitespace-nowrap text-ellipsis">{p.name}</h3>
-                  </div>
-                  <p className="text-xs overflow-hidden whitespace-nowrap text-ellipsis">
-                    Created by: <span className="text-base">{p.createdBy.username}</span>
-                  </p>
-                  <p className="text-xs font-jetbrains min-h-10">
-                    <span className="text-xs font-inter">Created: </span>
-                    {joinedDate ?? "-"}
-                  </p>
-                </div>
+		<div className="w-full h-full space-y-4">
+			<div className="space-y-5">
+				<h1 className="text-2xl font-bold">Projects</h1>
+				<button
+					type="button"
+					onClick={() => setIsOpen(true)}
+					className="bg-primary rounded-xl px-3 py-1 cursor-pointer text-sm text-white hover:opacity-80 transition-all duration-150">
+					Create Project
+				</button>
 
-              </Link>
-            );
-          })
-        }
+				<Activity mode={isOpen ? "visible" : "hidden"}>
+					<CreateProjectModal isOpen={isOpen} setIsOpen={setIsOpen} />
+				</Activity>
 
-      </div>
-    </div >
-  )
+				{data.length > 0 && (
+					<p className="text-xs">Select a project to continue</p>
+				)}
+			</div>
+			<div className="grid gap-3 sm:grid-cols-2 md:grid-cols-3">
+				{data?.map((p) => {
+					const joinedDate =
+						p.createdAt ? formatter.format(new Date(p.createdAt)) : null;
+					return (
+						<Link
+							to={`/workspaces/${p.workspaceId}/projects/${p.id}/tasks`}
+							key={p.id}
+							className="p-4 bg-card shadow-md shadow-gray-200 rounded-2xl border-2 border-transparent hover:border-2 hover:border-border transition-colors duration-150">
+							<div className="flex flex-col gap-3 items-start justify-between flex-wrap ">
+								<div className="w-full flex items-start justify-between gap-3">
+									<h3 className="text-base font-semibold  overflow-hidden whitespace-nowrap text-ellipsis">
+										{p.name}
+									</h3>
+								</div>
+								<p className="text-xs overflow-hidden whitespace-nowrap text-ellipsis">
+									Created by:{" "}
+									<span className="text-base">{p.createdBy.username}</span>
+								</p>
+								<p className="text-xs font-jetbrains min-h-10">
+									<span className="text-xs font-inter">Created: </span>
+									{joinedDate ?? "-"}
+								</p>
+							</div>
+						</Link>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
