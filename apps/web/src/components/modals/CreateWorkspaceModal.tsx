46f2581ec63c13bf9d24f0c64aff5fddf7ae4156
kiptalam1 <adamskiptalam0@gmail.com>
2026-03-07 14:43:47 +0300

    feat: implement workspace creation and refetch via query invalidation

--- apps/web/src/components/modals/CreateWorkspaceModal.tsx
+++ apps/web/src/components/modals/CreateWorkspaceModal.tsx
@@ -1,49 +1,75 @@
-import type { Dispatch, SetStateAction } from "react";
+import { useState, type Dispatch, type SetStateAction, type SyntheticEvent } from "react";
+import useCreateWorkspace from "../../features/workspaces/hooks/useCreateWorkspace";
+
 
 type ModalProps = {
-	isOpen: boolean;
-	setIsOpen: Dispatch<SetStateAction<boolean>>;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 };
-export default function CreateWorkspaceModal({
-	isOpen,
-	setIsOpen,
-}: ModalProps) {
-	if (!isOpen) {
-		return null;
-	}
 
-	return (
-		<div
-			onClick={() => setIsOpen(false)}
-			className="p-2 fixed bg-black/50 backdrop-blur-md inset-0 flex items-center justify-center">
-			<form
-				onClick={(e) => e.stopPropagation()}
-				className=" max-w-md w-full sm:w-sm md:w-md bg-card shadow-md shadow-gray-200 rounded-xl p-4">
-				<h2 className="font-semibold text-lg">Create a Workspace</h2>
-				<div className="flex flex-col my-5 gap-2 ">
-					<label htmlFor="name" className="text-sm">
-						Enter workspace name:
-					</label>
-					<input
-						id="name"
-						type="text"
-						className="border border-border rounded-lg outline-none focus:ring focus:ring-primary py-0.5 px-2 text-base"
-					/>
-				</div>
-				<div className="flex items-center justify-between gap-3">
-					<button
-						type="button"
-						onClick={() => setIsOpen(false)}
-						className="bg-inherit border border-muted text-text rounded-md px-1 py-0.5 text-sm hover:bg-red-400 hover:border-white transition-colors duration-150 cursor-pointer disabled:cursor-not-allowed">
-						Cancel
-					</button>
-					<button
-						type="submit"
-						className="bg-primary rounded-md text-white px-5 py-0.5 text-sm font-semibold hover:opacity-60 transition-opacity duration-150 cursor-pointer disabled:cursor-not-allowed">
-						Save
-					</button>
-				</div>
-			</form>
-		</div>
-	);
+type FormTypes = {
+  name: string;
+}
+
+export default function CreateWorkspaceModal({
+  isOpen,
+  setIsOpen,
+}: ModalProps) {
+
+  const [formData, setFormData] = useState<FormTypes>({
+    name: ""
+  })
+  const { mutate, isPending } = useCreateWorkspace()
+
+  if (!isOpen) {
+    return null;
+  }
+
+  function handleSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    mutate(formData.name, {
+      onSuccess: () => {
+        setIsOpen(false);
+      }
+    });
+  }
+  return (
+    <div
+      onClick={() => setIsOpen(false)}
+      className="p-2 fixed bg-black/50 backdrop-blur-md inset-0 flex items-center justify-center">
+      <form
+        onClick={(e) => e.stopPropagation()}
+        onSubmit={handleSubmit}
+        className=" max-w-md w-full sm:w-sm md:w-md bg-card shadow-md shadow-gray-200 rounded-xl p-4">
+        <h2 className="font-semibold text-lg">Create a Workspace</h2>
+        <div className="flex flex-col my-5 gap-2 ">
+          <label htmlFor="name" className="text-sm">
+            Enter workspace name:
+          </label>
+          <input
+            id="name"
+            type="text"
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            required
+            className="border border-border rounded-lg outline-none focus:ring focus:ring-primary py-0.5 px-2 text-base"
+          />
+        </div>
+        <div className="flex items-center justify-between gap-3">
+          <button
+            type="button"
+            onClick={() => setIsOpen(false)}
+            className="bg-inherit border border-muted text-text rounded-md px-1 py-0.5 text-sm hover:bg-red-400 hover:border-white transition-colors duration-150 cursor-pointer disabled:cursor-not-allowed">
+            Cancel
+          </button>
+          <button
+            type="submit"
+            disabled={isPending}
+            className="bg-primary rounded-md text-white px-5 py-0.5 text-sm font-semibold hover:opacity-60 transition-opacity duration-150 cursor-pointer disabled:cursor-not-allowed">
+            {isPending ? "Loading..." : "Save"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- apps/web/src/pages/WorkspacesPage.tsx
+++ apps/web/src/pages/WorkspacesPage.tsx
@@ -36,62 +36,62 @@ export default function WorkspacesPage() {
 
 
   return (
-		<div className="w-full h-full space-y-4">
-			<div className="space-y-5">
-				<h1 className="text-2xl font-bold">Workspaces</h1>
-				<button
-					onClick={() => setIsOpen(true)}
-					type="button"
-					className="bg-primary rounded-xl px-3 py-1 cursor-pointer text-sm text-white hover:opacity-80 transition-all duration-150">
-					Create workspace
-				</button>
+    <div className="w-full h-full space-y-4">
+      <div className="space-y-5">
+        <h1 className="text-2xl font-bold">Workspaces</h1>
+        <button
+          onClick={() => setIsOpen(true)}
+          type="button"
+          className="bg-primary rounded-xl px-3 py-1 cursor-pointer text-sm text-white hover:opacity-80 transition-all duration-150">
+          Create workspace
+        </button>
 
-				<Activity mode={isOpen ? "visible" : "hidden"}>
-					<CreateWorkspaceModal isOpen={isOpen} setIsOpen={setIsOpen} />
-				</Activity>
+        <Activity mode={isOpen ? "visible" : "hidden"}>
+          <CreateWorkspaceModal isOpen={isOpen} setIsOpen={setIsOpen} />
+        </Activity>
 
-				{data.length > 0 && (
-					<p className="text-xs">Select a workspace to continue</p>
-				)}
-			</div>
-			<div className="grid gap-3 sm:grid-cols-2 md:grid-cols-3">
-				{data?.map((w) => {
-					const wsUser = w.members.find((m) => m.userId === user?.id);
-					const joinedDate =
-						wsUser?.joinedAt ?
-							formatter.format(new Date(wsUser.joinedAt))
-						:	null;
-					return (
-						<Link
-							to={`${w.id}/projects`}
-							key={w.id}
-							className="p-4 bg-card shadow-md shadow-gray-200 rounded-2xl border-2 border-transparent hover:border-2 hover:border-border transition-colors duration-150">
-							<div className="flex flex-col gap-3 items-start justify-between flex-wrap ">
-								<div className="w-full flex items-start justify-between gap-3">
-									<h3 className="text-base font-semibold  overflow-hidden whitespace-nowrap text-ellipsis">
-										{w.name}
-									</h3>
-									<p className="text-xs border border-border py-0.5 px-1 rounded-full">
-										{(wsUser?.memberRole ?? "MEMBER").toLocaleLowerCase()}
-									</p>
-								</div>
-								<p
-									className={`text-xs ${wsUser?.status === "ACTIVE" ? "text-success" : ""} ${wsUser?.status === "REMOVED" ? "text-danger" : ""} ${wsUser?.status === "SUSPENDED" ? "text-yellow-500" : ""} ${wsUser?.status === "INVITED" ? "text-blue-500" : ""}`}>
-									{wsUser?.status ?? "-"}
-								</p>
-								<p className="text-sm">
-									{w.members.length}{" "}
-									{w.members.length === 1 ? "member" : "members"}
-								</p>
-								<p className="text-xs font-jetbrains min-h-10">
-									<span className="text-xs font-inter">Joined: </span>
-									{joinedDate ?? "-"}
-								</p>
-							</div>
-						</Link>
-					);
-				})}
-			</div>
-		</div>
-	);
+        {data.length > 0 && (
+          <p className="text-xs">Select a workspace to continue</p>
+        )}
+      </div>
+      <div className="grid gap-3 sm:grid-cols-2 md:grid-cols-3">
+        {data?.map((w) => {
+          const wsUser = w.members.find((m) => m.userId === user?.id);
+          const joinedDate =
+            wsUser?.joinedAt ?
+              formatter.format(new Date(wsUser.joinedAt))
+              : null;
+          return (
+            <Link
+              to={`${w.id}/projects`}
+              key={w.id}
+              className="p-4 bg-card shadow-md shadow-gray-200 rounded-2xl border-2 border-transparent hover:border-2 hover:border-border transition-colors duration-150">
+              <div className="flex flex-col gap-3 items-start justify-between flex-wrap ">
+                <div className="w-full flex items-start justify-between gap-3">
+                  <h3 className="text-base font-semibold  overflow-hidden whitespace-nowrap text-ellipsis">
+                    {w.name}
+                  </h3>
+                  <p className="text-xs border border-border py-0.5 px-1 rounded-full">
+                    {(wsUser?.memberRole ?? "MEMBER").toLocaleLowerCase()}
+                  </p>
+                </div>
+                <p
+                  className={`text-xs ${wsUser?.status === "ACTIVE" ? "text-success" : ""} ${wsUser?.status === "REMOVED" ? "text-danger" : ""} ${wsUser?.status === "SUSPENDED" ? "text-yellow-500" : ""} ${wsUser?.status === "INVITED" ? "text-blue-500" : ""}`}>
+                  {wsUser?.status ?? "-"}
+                </p>
+                <p className="text-sm">
+                  {w.members.length}{" "}
+                  {w.members.length === 1 ? "member" : "members"}
+                </p>
+                <p className="text-xs font-jetbrains min-h-10">
+                  <span className="text-xs font-inter">Joined: </span>
+                  {joinedDate ?? "-"}
+                </p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
