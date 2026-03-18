7736eb1e6a6a2b7430166ea7ff8f9a83198af971
kiptalam1 <adamskiptalam0@gmail.com>
2026-03-18 20:21:00 +0300

    feat: implement invites sending function on frontend

--- apps/web/src/components/modals/CreateTaskModal.tsx
+++ apps/web/src/components/modals/CreateTaskModal.tsx
@@ -37,43 +37,45 @@ export default function CreateTaskModal({ isOpen, setIsOpen }: ModalProps) {
   }
 
   return (
-    <div
-      onClick={() => setIsOpen(false)}
-      className="p-2 fixed bg-black/50 backdrop-blur-md inset-0 flex items-center justify-center">
-      <form
-        onClick={(e) => e.stopPropagation()}
-        onSubmit={handleSubmit}
-        className=" max-w-md w-full sm:w-sm md:w-md bg-card shadow-md shadow-gray-200 rounded-xl p-4">
-        <h2 className="font-semibold text-lg">Create a Task</h2>
-        <div className="flex flex-col my-5 gap-2 ">
-          <label htmlFor="name" className="text-sm">
-            Enter Task title:
-          </label>
-          <input
-            id="title"
-            type="text"
-            name="title"
-            value={formData.title}
-            onChange={(e) => setFormData({ ...formData, title: e.target.value })}
-            className="border border-border rounded-lg outline-none focus:ring focus:ring-primary py-0.5 px-2 text-base"
-          />
-        </div>
-        <div className="flex items-center justify-between gap-3">
-          <button
-            type="button"
-            disabled={isPending}
-            onClick={() => setIsOpen(false)}
-            className="bg-inherit border border-muted text-text rounded-md px-1 py-0.5 text-sm hover:bg-red-400 hover:border-white transition-colors duration-150 cursor-pointer disabled:cursor-not-allowed">
-            Cancel
-          </button>
-          <button
-            type="submit"
-            disabled={isPending}
-            className="bg-primary rounded-md text-white px-5 py-0.5 text-sm font-semibold hover:opacity-60 transition-opacity duration-150 cursor-pointer disabled:cursor-not-allowed">
-            {isPending ? "Loading..." : "Save"}
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+		<div
+			onClick={() => setIsOpen(false)}
+			className="p-2 fixed bg-black/50 backdrop-blur-md inset-0 flex items-center justify-center">
+			<form
+				onClick={(e) => e.stopPropagation()}
+				onSubmit={handleSubmit}
+				className=" max-w-md w-full sm:w-sm md:w-md bg-card shadow-md shadow-gray-200 rounded-xl p-4">
+				<h2 className="font-semibold text-lg">Create a Task</h2>
+				<div className="flex flex-col my-5 gap-2 ">
+					<label htmlFor="title" className="text-sm">
+						Enter Task title:
+					</label>
+					<input
+						id="title"
+						type="text"
+						name="title"
+						value={formData.title}
+						onChange={(e) =>
+							setFormData({ ...formData, title: e.target.value })
+						}
+						className="border border-border rounded-lg outline-none focus:ring focus:ring-primary py-0.5 px-2 text-base"
+					/>
+				</div>
+				<div className="flex items-center justify-between gap-3">
+					<button
+						type="button"
+						disabled={isPending}
+						onClick={() => setIsOpen(false)}
+						className="bg-inherit border border-muted text-text rounded-md px-1 py-0.5 text-sm hover:bg-red-400 hover:border-white transition-colors duration-150 cursor-pointer disabled:cursor-not-allowed">
+						Cancel
+					</button>
+					<button
+						type="submit"
+						disabled={isPending}
+						className="bg-primary rounded-md text-white px-5 py-0.5 text-sm font-semibold hover:opacity-60 transition-opacity duration-150 cursor-pointer disabled:cursor-not-allowed">
+						{isPending ? "Loading..." : "Save"}
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
--- apps/web/src/components/modals/InviteMembersModal.tsx
+++ apps/web/src/components/modals/InviteMembersModal.tsx
@@ -1,5 +1,14 @@
 import { X } from "lucide-react";
-import { useState, type ClipboardEvent, type Dispatch, type KeyboardEvent, type SetStateAction } from "react";
+import {
+	useState,
+	type ClipboardEvent,
+	type Dispatch,
+	type KeyboardEvent,
+	type SetStateAction,
+	type SyntheticEvent,
+} from "react";
+import useSendWsInvite from "../../features/invites/hooks/useSendWsInvite";
+import { useParams } from "react-router";
 
 type ModalProps = {
   open: boolean;
@@ -10,6 +19,8 @@ export default function InviteMembersModal({ open, setOpen }: ModalProps) {
   const [emails, setEmails] = useState<string[]>([]);
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
   const MAX_INVITES = 20;
+  const { workspaceId } = useParams();
+	const { mutate: sendInvite, isPending: isSendingInvite } = useSendWsInvite();
 
   function handleKeyDown(event: KeyboardEvent<HTMLInputElement>) {
     if (event.key === "Enter" || event.key === "," || event.key === " ") {
@@ -56,55 +67,80 @@ export default function InviteMembersModal({ open, setOpen }: ModalProps) {
     setEmails((prev) => prev.filter(e => e !== email));
   }
 
+  function handleWsInvite(event: SyntheticEvent<HTMLFormElement>) {
+		event.preventDefault();
+		if (!workspaceId) return;
+		sendInvite(
+			{ workspaceId, emails },
+			{
+				onSuccess: () => {
+					setOpen(false);
+					setInput("");
+				},
+			},
+		);
+	}
+
   if (!open) {
     return null;
   }
   return (
-    <div
-      onClick={() => setOpen(false)}
-      className="fixed inset-0 bg-black/50 z-50 backdrop-blur-md flex items-center justify-center w-full h-full p-4">
-      <form
-        onClick={(e) => e.stopPropagation()}
-        className="w-full sm:max-w-sm md:max-w-md lg:max-w-lg flex flex-col gap-2 p-4 bg-card rounded-xl shadow-[0_-6px_12px_-6px_rgba(255,255,255,0.25)]">
-        <div className="flex items-center justify-between">
-          <h2 className="text-xl font-semibold">Invite members</h2>
-          <button
-            type="button"
-            onClick={() => setOpen(false)}>
-            <X className="hover:scale-110 transition-transform duration-150 cursor-pointer" size={20} />
-          </button>
-        </div>
-        <p className="text-xs text-muted italic">{emails.length}/{MAX_INVITES} invites</p>
-        <div className="flex flex-wrap gap-2 p-2 max-h-60 overflow-y-auto ">
-          {
-            emails.map((email) => (
-              <span
-                key={email}
-                className="text-xs sm:text-sm font-jetbrains flex items-center gap-1 rounded-2xl px-2 py-0.5 bg-gray-200 max-w-full">
-                <span className="truncate wrap-break-word max-w-35 sm:max-w-50">{email}</span>
-                <button
-                  type="button"
-                  className="shrink-0 cursor-pointer"
-                  onClick={() => removeEmail(email)}>
-                  <X size={16} />
-                </button>
-              </span>
-            ))
-          }
-          <input
-            type="email"
-            disabled={emails.length >= MAX_INVITES}
-            onKeyDown={handleKeyDown}
-            onPaste={handlePasteEmails}
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="Separate emails with commas or spaces..."
-            className="flex-1 min-w-full w-full border-2 border-border my-1 rounded-lg outline-none focus:ring ring-primary p-2 text-sm placeholder:text-xs placeholder:font-jetbrains placeholder:italic placeholder:text-gray-500 font-light "></input>
-          <button
-            disabled={emails.length === 0}
-            className="bg-primary rounded-lg px-2 py-1 text-sm text-white w-full my-2 hover:opacity-80 cursor-pointer transition-opacity duration-150 disabled:opacity-50 disabled:cursor-not-allowed">Invite</button>
-        </div>
-      </form>
-    </div>
-  )
+		<div
+			onClick={() => setOpen(false)}
+			className="fixed inset-0 bg-black/50 z-50 backdrop-blur-md flex items-center justify-center w-full h-full p-4">
+			<form
+				onSubmit={handleWsInvite}
+				onClick={(e) => e.stopPropagation()}
+				className="w-full sm:max-w-sm md:max-w-md lg:max-w-lg flex flex-col gap-2 p-4 bg-card rounded-xl shadow-[0_-6px_12px_-6px_rgba(255,255,255,0.25)]">
+				<div className="flex items-center justify-between">
+					<h2 className="text-xl font-semibold">Invite members</h2>
+					<button
+						type="button"
+						aria-label="close form"
+						onClick={() => setOpen(false)}>
+						<X
+							className="hover:scale-110 transition-transform duration-150 cursor-pointer"
+							size={20}
+						/>
+					</button>
+				</div>
+				<p className="text-xs text-muted italic">
+					{emails.length}/{MAX_INVITES} invites
+				</p>
+				<div className="flex flex-wrap gap-2 p-2 max-h-60 overflow-y-auto ">
+					{emails.map((email) => (
+						<span
+							key={email}
+							className="text-xs sm:text-sm font-jetbrains flex items-center gap-1 rounded-2xl px-2 py-0.5 bg-gray-200 max-w-full">
+							<span className="truncate wrap-break-word max-w-35 sm:max-w-50">
+								{email}
+							</span>
+							<button
+								type="button"
+								aria-label="remove email"
+								className="shrink-0 cursor-pointer"
+								onClick={() => removeEmail(email)}>
+								<X size={16} />
+							</button>
+						</span>
+					))}
+					<input
+						type="email"
+						disabled={emails.length >= MAX_INVITES}
+						onKeyDown={handleKeyDown}
+						onPaste={handlePasteEmails}
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						placeholder="Separate emails with commas or spaces..."
+						className="flex-1 min-w-full w-full border-2 border-border my-1 rounded-lg outline-none focus:ring ring-primary p-2 text-sm placeholder:text-xs placeholder:font-jetbrains placeholder:italic placeholder:text-gray-500 font-light "></input>
+					<button
+						type="submit"
+						disabled={emails.length === 0}
+						className="bg-primary rounded-lg px-2 py-1 text-sm text-white w-full my-2 hover:opacity-80 cursor-pointer transition-opacity duration-150 disabled:opacity-50 disabled:cursor-not-allowed">
+						{isSendingInvite ? "Inviting..." : "Invite"}
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
