49977e261dfdefea2f59f0bdbc9bc85a881c1719
kiptalam1 <adamskiptalam0@gmail.com>
2026-03-25 15:20:24 +0300

    create endpoint to decline ws-invite

--- apps/server/src/routes/invite.routes.ts
+++ apps/server/src/routes/invite.routes.ts
@@ -1,19 +1,21 @@
 import express from "express";
 import {
-	acceptWsInvite,
-	getWorkspaceInvites,
-	sendWorkspaceInvite,
+  acceptWsInvite,
+  getWorkspaceInvites,
+  rejectWsInvite,
+  sendWorkspaceInvite,
 } from "../controllers/invite.controllers.js";
 import { isAuthenticated } from "../middlewares/auth.middlewares.js";
 
 const router = express.Router();
 
 router.post(
-	"/workspace/:workspaceId/invite",
-	isAuthenticated,
-	sendWorkspaceInvite,
+  "/workspace/:workspaceId/invite",
+  isAuthenticated,
+  sendWorkspaceInvite,
 );
 router.get("/", isAuthenticated, getWorkspaceInvites);
 router.post("/:token/accept", isAuthenticated, acceptWsInvite);
+router.post("/:token/decline", isAuthenticated, rejectWsInvite);
 
 export default router;
