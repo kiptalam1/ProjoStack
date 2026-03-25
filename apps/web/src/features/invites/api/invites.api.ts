37e28906a12c9bc41d29a83075c02752bb5f84b1
kiptalam1 <adamskiptalam0@gmail.com>
2026-03-25 12:04:42 +0300

    feat: create function to target accept-invite api

--- apps/web/src/features/invites/api/invites.api.ts
+++ apps/web/src/features/invites/api/invites.api.ts
@@ -3,47 +3,67 @@ import type { InviteTypes } from "./invites.types";
 import api from "../../../api/api";
 
 export async function getWsInvites(): Promise<InviteTypes[]> {
-	try {
-		const res = await api.get("/invites");
-		return res.data.data;
-	} catch (error: unknown) {
-		if (error instanceof AxiosError) {
-			throw new Error(error.response?.data.error);
-		}
+  try {
+    const res = await api.get("/invites");
+    return res.data.data;
+  } catch (error: unknown) {
+    if (error instanceof AxiosError) {
+      throw new Error(error.response?.data.error);
+    }
 
-		if (error instanceof Error) {
-			throw Error(error.message);
-		}
-		throw new Error("Unexpected error occurred");
-	}
+    if (error instanceof Error) {
+      throw Error(error.message);
+    }
+    throw new Error("Unexpected error occurred");
+  }
 }
 
 type ResponseTypes = {
-	message: string;
-	data: InviteTypes;
+  message: string;
+  data: InviteTypes;
 };
 
 type InputTypes = {
-	workspaceId: string;
-	emails: string[];
+  workspaceId: string;
+  emails: string[];
 };
 export async function sendWsInvite({
-	workspaceId,
-	emails,
+  workspaceId,
+  emails,
 }: InputTypes): Promise<ResponseTypes> {
-	try {
-		const res = await api.post(`/invites/workspace/${workspaceId}/invite`, {
-			emails,
-		});
-		return res.data;
-	} catch (error: unknown) {
-		if (error instanceof AxiosError) {
-			throw new Error(error.response?.data.error);
-		}
+  try {
+    const res = await api.post(`/invites/workspace/${workspaceId}/invite`, {
+      emails,
+    });
+    return res.data;
+  } catch (error: unknown) {
+    if (error instanceof AxiosError) {
+      throw new Error(error.response?.data.error);
+    }
 
-		if (error instanceof Error) {
-			throw Error(error.message);
-		}
-		throw new Error("Unexpected error occurred");
-	}
+    if (error instanceof Error) {
+      throw Error(error.message);
+    }
+    throw new Error("Unexpected error occurred");
+  }
 }
+
+// accept invite;
+export async function acceptWsInvite(
+  token: string,
+): Promise<{ message: string }> {
+  try {
+    const res = await api.post(`/invites/${token}/accept`);
+    return res.data;
+  } catch (error: unknown) {
+    if (error instanceof AxiosError) {
+      throw new Error(error.response?.data.error);
+    }
+
+    if (error instanceof Error) {
+      throw Error(error.message);
+    }
+    throw new Error("Unexpected error occurred");
+  }
+}
+
