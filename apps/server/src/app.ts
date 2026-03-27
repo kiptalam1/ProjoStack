9fd98927d51047841b2ac14e52811320ca3fb501
kiptalam1 <adamskiptalam0@gmail.com>
2026-03-27 18:36:36 +0300

    fix: manually create __dirname since not available in ESM

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
@@ -39,8 +40,12 @@ app.use("/api/workspaces", apiLimiter, workspaceRoutes);
 app.use("/api/projects", apiLimiter, projectRoutes);
 app.use("/api/tasks", apiLimiter, taskRoutes);
 app.use("/api/invites", apiLimiter, inviteRoutes);
-//
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// serve frontend;
 app.use(express.static(path.join(__dirname, "../../web/dist")));
+// SPA fallback;
 app.get("*", (_req, res) => {
 	res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
 });
