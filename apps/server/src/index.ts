8a57efe4f11196f19862f8715c6f002203dc1418
kiptalam1 <adamskiptalam0@gmail.com>
2026-03-27 22:02:11 +0300

    fix: trust proxy configuration

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,5 +3,6 @@ dotenv.config();
 import app from "./app.js";
 const PORT = process.env.PORT || 5000;
 
+app.set("trust proxy", 1);
 // Listen to app at port;
 app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
